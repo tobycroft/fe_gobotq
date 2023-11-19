b529c4568da45f26e2c58f6f737682a0b842a328
tuuz <silverhawk@foxmail.com>
2023-11-19 23:56:03 +0800

    assets favicon.ico index.html

---
+++ assets/ssrBoot-5dced86a.js
@@ -0,0 +1,1 @@
+import{ax as t,aL as e,c as s,bh as n}from"./index-0deaa98d.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
