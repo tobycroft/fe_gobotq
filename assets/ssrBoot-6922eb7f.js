814f61fb5a1462de3741478b59a65cb620fd2669
tuuz <silverhawk@foxmail.com>
2023-11-25 17:48:47 +0800

    TuuzPC

---
+++ assets/ssrBoot-6922eb7f.js
@@ -0,0 +1,1 @@
+import{ax as t,aL as e,c as s,bd as n}from"./index-611e515b.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
